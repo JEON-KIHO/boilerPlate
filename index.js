257d4ab9268f62bc342e2a8d46fcf6393ece66bc
JEONKIHO <rlghakswo1@gmail.com>
2021-05-26 13:55:00 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 const mongoose = require('./mongoose');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 mongoose.then(() => console.log('MongoDB Connected......'))
     .catch(err => console.log('err......... : ' + err))
@@ -9,7 +14,16 @@ mongoose.then(() => console.log('MongoDB Connected......'))
 
 
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.send('hello!!!');
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, info) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    });
 })
 
 app.listen(port, () => { console.log(`connected port:${port}`) });
--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const config = require('./config/Key');
 
-const connect = mongoose.connect('mongodb+srv://root:1234@boilerplate.8maqq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const connect = mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false,
 })
 
